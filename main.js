95da3d0a920c3eb128abc70b2532ce92806a9ebf
CrypticiaN <obscura.darkside@gmail.com>
2021-11-02 15:09:08 +0100

    lista de tareas, formateo css y funciones botones de añadir y borrar tareas

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    //write code inside this block
+
+    let tasks = [];
+
+// ads task to task list
+    $("#submit").on("click", function() {
+        let newTask = $("#newTask").val();
+
+        if (newTask !== "") {
+
+            tasks.push(newTask);
+
+            $("#taskList").empty();
+
+            tasks.forEach( t =>
+                $("#taskList").append(
+                    `<div class="task">${t}</div>`
+                )
+            );
+        }
+    });
+
+// clear all list items
+    $("#clearButton").on("click", function() {
+       $("#taskList").empty()
+    });
+});
+
